fc0d0193ffcda854f7fb236ead87bae4c9dd0d6e
Gabriele Ara <gabriele.ara@santannapisa.it>
2021-04-14 13:54:20 +0200

    Updated simulation table generation

--- host/pyscripts/simtable.py
+++ host/pyscripts/simtable.py
@@ -3,10 +3,27 @@
 import argparse
 import os
 from pathlib import Path
-import pandas as pd
-import numpy as np
 
-# TODO: power, voltage... but also temperature!
+import numpy as np
+import pandas as pd
+
+# +--------------------------------------------------------+
+# |                       Constants                        |
+# +--------------------------------------------------------+
+
+ISLAND  = 'island'
+FREQ    = 'frequency'
+TASK    = 'task'
+HOWMANY = 'howmany'
+TIME    = 'time_mean'
+POWER   = 'power_mean'
+IDLE    = 'idle'
+
+SINGLE      = 'single'
+AVERAGE     = 'average'
+MAXIMUM     = 'maximum'
+TRUE_REG    = 'true_regression'
+FIXED_REG   = 'fixed_regression'
 
 # +--------------------------------------------------------+
 # |          Command-line Arguments Configuration          |
@@ -18,7 +35,35 @@ options = [
         'long': 'in_file',
         'opts': {
             'metavar': 'in-file',
-            'type': argparse.FileType('r'),
+            'type': str,
+        },
+    },
+    {
+        'short': '-t',
+        'long': '--time-fun',
+        'opts': {
+            'help': 'The method used to generate the time column',
+            'type': str,
+            'choices': [
+                SINGLE,
+                AVERAGE,
+                MAXIMUM,
+            ],
+            'default': SINGLE,
+        },
+    },
+    {
+        'short': '-p',
+        'long': '--power-fun',
+        'opts': {
+            'help': 'The method used to generate the power column',
+            'type': str,
+            'choices': [
+                SINGLE,
+                TRUE_REG,
+                FIXED_REG,
+            ],
+            'default': SINGLE,
         },
     },
     {
@@ -29,33 +74,6 @@ options = [
             'type': str,
             'default': 'a.out',
         },
-    },
-    {
-        'short': '-t',
-        'long': '--time-coeff',
-        'opts': {
-            'help': 'The method to use to calculate the time coefficient.',
-            'type': str,
-            'choices': [
-                'one',
-                'avg',
-            ],
-            'default': 'one',
-        }
-    },
-    {
-        'short': '-p',
-        'long': '--power-coeff',
-        'opts': {
-            'help': 'The method to use to calculate the power coefficients.',
-            'type': str,
-            'choices': [
-                'one',
-                'regression',
-                'fixed_regression',
-            ],
-            'default': 'one',
-        }
     },
 ]
 
@@ -74,8 +92,9 @@ def parse_cmdline_args():
     return parser.parse_args()
 #-- parse_cmdline_args
 
-# ----------------------------------------------------------
-
+# +--------------------------------------------------------+
+# |                      Save to CSV                       |
+# +--------------------------------------------------------+
 
 def safe_save_to_csv(out_df, out_file):
     # Create a temporary file in the destination mount fs
@@ -83,8 +102,6 @@ def safe_save_to_csv(out_df, out_file):
     out_dir = os.path.dirname(os.path.abspath(out_file))
     Path(out_dir).mkdir(parents=True, exist_ok=True)
     tmpfile_name = out_dir + '/raw_' + str(os.getpid()) + '.tmp'
-    out_df.to_csv(tmpfile_name, index=None)
-
     out_df.to_csv(tmpfile_name, index=None)
 
     # NOTE: It should be safe this way, but otherwise please
@@ -96,202 +113,239 @@ def safe_save_to_csv(out_df, out_file):
     # (or don't, the program will terminate anyway)
 #-- safe_save_to_csv
 
-#----------------------------------------------------------#
-
-# NOTICE: this is fixed to using mean values, if you want to
-# use the medians instead you should change _mean to _50% in
-# these string constants
+# +--------------------------------------------------------+
+# |                     Time Functions                     |
+# +--------------------------------------------------------+
 
 
-HOWMANY = 'howmany'
-ISLAND = 'island'
-FREQ = 'frequency'
-POWER = 'power_mean'
-TIME = 'time_rel'
-VOLTAGE = 'voltage_mean'
-POWER_M = 'power_m'
-POWER_Q = 'power_q'
-SLOWNESS = 'slowness'
-IDLE = 'idle'
-TASK = 'task'
+def time_max(df, island, frequency, task):
+    """
+    Maximum (average) runtime profiled.
 
-OUTCOLS = [
-    ISLAND,
-    FREQ,
-    TASK,
-    SLOWNESS,
-    POWER_M,
-    POWER_Q,
-]
-
-#----------------------------------------------------------#
-
-
-def coeff_time_one(df, island, frequency, task):
-    """Calculate the expected runtime as the value measured
-    for the single task execution experiment.
+    Returns the maximum execution time registered for the workload task in the
+    given island+frequency configuration when varying number of concurrent
+    tasks.
     """
 
-    if task == IDLE:
+    if (task == IDLE):
         return 0
     df = df[df[ISLAND] == island]
     df = df[df[FREQ] == frequency]
     df = df[df[TASK] == task]
-    df = df[df[HOWMANY] == 1]
-    return df[TIME].values[0]
-#-- coeff_time_one
-
-
-def coeff_time_avg(df, island, frequency, task):
-    """Calculate the expected runtime as the AVERAGE value
-    measured for an increasing number of parallel tasks
-    during experimentation.
-    """
-
-    if task == IDLE:
-        return 0
-    df = df[df[ISLAND] == island]
-    df = df[df[FREQ] == frequency]
-    df = df[df[TASK] == task]
-    values = df[TIME].values
-    return np.mean(values)
+    return df[TIME].max()
+    # howmanies = df[HOWMANY].unique()
     # l = []
-    # for h in df[HOWMANY].unique():
-    #     l += [df[df[HOWMANY] == h][TIME].values[0]]
+    # for h in howmanies:
+    #     l += [df[df[HOWMANY] == h]['time_avg'].values[0]]
+    # return max(l)
+#-- time_max
+
+
+def time_single(df, island, frequency, task):
+    """
+    Runtime profiled using only one task in isolation.
+    """
+    if (task == IDLE):
+        return 0
+    df = df[df[ISLAND] == island]
+    df = df[df[FREQ] == frequency]
+    df = df[df[TASK] == task]
+    return df[df[HOWMANY] == 1][TIME].values[0]
+#-- time_one
+
+
+def time_mean(df, island, frequency, task):
+    """
+    Average runtime profiled using an increasing number of parallel tasks.
+    """
+    if (task == IDLE):
+        return 0
+    df = df[df[ISLAND] == island]
+    df = df[df[FREQ] == frequency]
+    df = df[df[TASK] == task]
+    return df[TIME].mean()
+    # howmanies = df[HOWMANY].unique()
+    # l = []
+    # for h in howmanies:
+    #     l += [df[df[HOWMANY] == h]['time_avg'].values[0]]
     # return sum(l) / (1.0 * len(l))
-#-- coeff_time_avg
+#-- time_mean
 
 
-def coeff_power_one(df, island, frequency, task):
-    """Calculate the expected power consumption m and q
-    values as the difference between the value measured for
-    the single task execution experiment and the measurement
-    for the IDLE task.
+# +--------------------------------------------------------+
+# |                    Power Functions                     |
+# +--------------------------------------------------------+
+
+# def power_rtsim_wrong(df, island, frequency, task):
+#     df = df[df[ISLAND] == island]
+#     df = df[df[FREQ] == frequency]
+#     m = df[df[HOWMANY] == 1][POWER].values[0]
+#     return m, 0
+
+# NOTICE: the q values are only for debug purposes, EXCEPT FOR THE IDLE POWER!
+
+
+def power_idle(df):
     """
+    Returns power of idle task (assumes core type and frequency to be fixed)
+    """
+    df = df[df[TASK] == IDLE]
+    return df[df[HOWMANY] == 1][POWER].values[0]
+#-- power_idle
 
+
+def power_single(df, island, frequency, task):
+    """
+    Returns the m and q coefficients obtained running a single task in
+    isolation.
+
+    NOTICE: the simulator will ignore this q value and use the IDLE power
+    anyway.
+    """
     df = df[df[ISLAND] == island]
     df = df[df[FREQ] == frequency]
-    idle = df
-    idle = idle[idle[TASK] == IDLE]
-    idle = idle[idle[HOWMANY] == 1]
 
-    q = idle[POWER].values[0]
+    pidle = power_idle(df)
+    if (task == IDLE):
+        return 0, pidle
 
     df = df[df[TASK] == task]
-    df = df[df[HOWMANY] == 1]
-    m = df[POWER].values[0] - q
 
+    q = pidle
+    m = df[df[HOWMANY] == 1][POWER].values[0] - pidle
     return m, q
-#-- coeff_power_one
+#-- power_single
 
 
-def fill_x_y(df, island, frequency, task):
-    """Fill x and y arrays with the power values collected
-    during experiment. First value is always (0, P_idle),
-    while the ones after that are the values measured for an
-    increasing number of parallel tasks.
+def power_getxy(df, pidle):
+    # x.append(0)
+    # y.append(pidle)
+    # for i in range(1, 5):
+    #     x.append(i)
+    #     y.append(df[df[HOWMANY] == i][POWER].values[0])
+    # x = np.array(x)
+    # y = np.array(y)
 
-    Returns: x, y
-    """
-
-    df = df
-    df = df[df[ISLAND] == island]
-    df = df[df[FREQ] == frequency]
-    idle = df
-    idle = idle[idle[TASK] == IDLE]
-    idle = idle[idle[HOWMANY] == 1]
-
-    df = df[df[TASK] == task]
-    howmanies = df[HOWMANY].unique()
-
-    x = np.empty(len(howmanies) + 1)
+    numcores = df[HOWMANY].size
+    x = np.arange(numcores+1)
     y = np.empty_like(x)
+    y[0] = pidle
+    for i in x[1:]:
+        y[i] = df[df[HOWMANY] == i][POWER].values[0]
 
-    i = 0
-    x[i] = 0
-    y[i] = idle[POWER].values[0]
-    for h in howmanies:
-        i += 1
-        x[i] = h
-        y[i] = df[df[HOWMANY] == h][POWER].values[0]
-
-    return x, y
-#-- fill_x_y
+    return (x, y)
+#-- power_getxy
 
 
-def coeff_power_regression(df, island, frequency, task):
-    """Calculate the expected power consumption m and q
-    values using linear regression of the values collected
-    for an increasing number of parallel tasks.
-    """
-
-    x, y = fill_x_y(df, island, frequency, task)
+def true_regression_fun(x, y):
     m, q = np.polyfit(x, y, 1)
     return m, q
-#-- coeff_power_regression
 
 
-def coeff_power_fixed_regression(df, island, frequency, task):
-    """Calculate the expected power consumption m and q
-    values using a custom regression for a fixed q value
-    equal to the value measured for the IDLE task, using
-    then the values collected for an increasing number of
-    parallel tasks to calculate the m.
-    """
-
-    x, y = fill_x_y(df, island, frequency, task)
-    q = y[0]
-    m = 1 / np.dot(x, x) * (np.dot(x, y) - q * sum(x))
+def fixed_regression_fun(x, y):
+    q = x[0]
+    m = 1 / np.dot(x, x) * (np.dot(x, y) - q * x.sum())
     return m, q
-#-- coeff_power_fixed_regression
 
 
-#----------------------------------------------------------#
-
-# TODO: how do I prepare more efficiently an array of rows?
-def prepare_simtable(df, coeff_power, coeff_time):
-    """Prepare a simulation table using the two given
-    functions to calculate the power and the time
-    coefficients to fill the table.
+def power_regression(df, island, frequency, task, regfun):
     """
+    Returns m and q values obtained using the regression function in input.
+    """
+    df = df[df[ISLAND] == island]
+    df = df[df[FREQ] == frequency]
 
-    # outdf = pd.DataFrame(columns=OUTCOLS)
-    rows = []
-    for i in df[ISLAND].unique():
-        for t in df[TASK].unique():
-            for f in df[df[ISLAND] == i][FREQ].unique():
-                pm, pq = coeff_power(df, i, f, t)
-                time = coeff_time(df, i, f, t)
-                rows += [[i, f, t, time, pm, pq]]
-                # df2 = pd.DataFrame([row], columns=cols)
-                # outdf = outdf.append(df2, ignore_index=True)
-    outdf = pd.DataFrame(rows, columns=OUTCOLS)
-    return outdf
-# -- prepare_simtable
+    pidle = power_idle(df)
+    if (task == IDLE):
+        return 0, pidle
+
+    df = df[df[TASK] == task]
+    (x, y) = power_getxy(df, pidle)
+    return regfun(x, y)
+#-- power_regression
 
 
-#----------------------------------------------------------#
+def power_true_regression(df, island, frequency, task):
+    """
+    Returns the m and q coefficients obtained using true regression on the
+    measured values (including idle).
 
+    NOTICE: the simulator will ignore this q value and use the IDLE power
+    instead.
+    """
+    return power_regression(df, island, frequency, task, true_regression_fun)
+#-- power_true_regression
+
+
+def power_fixed_regression(df, island, frequency, task):
+    """
+    Returns the m and q coefficients obtained using a fixed regression, in which q is forced to be equal to the IDLE power.
+
+    NOTICE: the simulator will ignore this q value and use the IDLE power
+    anyway.
+    """
+    return power_regression(df, island, frequency, task, fixed_regression_fun)
+#-- power_fixed_regression
+
+
+# +--------------------------------------------------------+
+# |                    Prepare Simtable                    |
+# +--------------------------------------------------------+
+
+
+def simulation_table(df, time_fun, power_fun):
+    islands = df[ISLAND].unique()
+    tasks = df[TASK].unique()
+
+    # TODO: add voltage info
+    # cols = [ISLAND, FREQ, TASK, 'power', 'time']
+
+    dictionary_list = []
+    for i in islands:
+        for t in tasks:
+            freqs = df[df[ISLAND] == i][FREQ].unique()
+            for f in freqs:
+                time = time_fun(df, i, f, t)
+                power, pidle = power_fun(df, i, f, t)
+                if (t == IDLE):
+                    power = pidle
+
+                dictionary_data = {
+                    ISLAND: i,
+                    FREQ: f,
+                    TASK: t,
+                    'power': power,
+                    'time': time,
+                }
+                dictionary_list.append(dictionary_data)
+
+    return pd.DataFrame.from_dict(dictionary_list)
+#-- simulation_table
+
+# +--------------------------------------------------------+
+# |                          Main                          |
+# +--------------------------------------------------------+
 
 def main():
     args = parse_cmdline_args()
-
-    time_coefficients = {
-        'one': coeff_time_one,
-        'avg': coeff_time_avg,
-    }
-
-    power_coefficients = {
-        'one': coeff_power_one,
-        'regression': coeff_power_regression,
-        'fixed_regression': coeff_power_fixed_regression,
-    }
-
-    coeff_time = time_coefficients[args.time_coeff]
-    coeff_power = power_coefficients[args.power_coeff]
-
     df = pd.read_csv(args.in_file)
-    out_df = prepare_simtable(df, coeff_power, coeff_time)
+
+    time_funs = {
+        SINGLE: time_single,
+        AVERAGE: time_mean,
+        MAXIMUM: time_max,
+    }
+
+    power_funs = {
+        SINGLE: power_single,
+        TRUE_REG: power_true_regression,
+        FIXED_REG: power_fixed_regression,
+    }
+
+    time_fun = time_funs[args.time_fun]
+    power_fun = power_funs[args.power_fun]
+
+    out_df = simulation_table(df, time_fun, power_fun)
     safe_save_to_csv(out_df, args.out_file)
 
     return 0
